726c59f30c4842dfb50e6d5e190962eb32454297
Krishna Sagar R <ragas.sagar.474@gmail.com>
2019-12-27 14:25:49 +0000

    Update build-changed.js

--- scripts/build-changed.js
+++ scripts/build-changed.js
@@ -2,8 +2,8 @@ const { execSync } = require('child_process');
 const { readFileSync } = require('fs');
 const path = require('path');
 
-const changes = readFileSync(path.resolve(__dirname,'..','changed.json'));
-
+const changesFileObj = readFileSync(path.resolve(__dirname,'..','changed.json'));
+const changes = changesFileObj ? JSON.paese(changesFileObj.toString()) : [];
 changes.forEach((change) => {
    execSync(`lerna exec --scope ${change.name} npm run pack`,{
      cwd: process.cwd(),
